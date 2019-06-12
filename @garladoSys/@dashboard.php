6eba73b1fd8409da34ae66f0d86430fdce896aa3
Kevo Kario <kevokario@gmail.com>
2019-06-12 11:52:29 +0300

    Work in progress

--- @garladoSys/@dashboard.php
+++ @garladoSys/@dashboard.php
@@ -12,11 +12,11 @@ if (isset($_SESSION['marvel']) === false) {
 <script src="Ajax/@dashboardloader.js" type="text/javascript"></script>
 <script src="Ajax/@dashboardPhp.js" type="text/javascript"></script>
 <style>
-    
+
 </style>
 </head>
 <?php
- // change onload to menuClicked('defaultPage') after deployment
+// change onload to menuClicked('defaultPage') after deployment
 ?>
 <body onload="menuClicked('defaultPage')">
     <div class="container-fluid jumbotron">
@@ -44,121 +44,121 @@ if (isset($_SESSION['marvel']) === false) {
     ============================================================================================
                                     MY NAVIGATION BAR
     ============================================================================================
-   -->
+    -->
 
     <nav class="nav navbar-default">
-<div class="container-fluid bg1">
-    <div class="nav navbar-header">
-        <button type="button" data-toggle="collapse" class="navbar-toggle" data-target="#mynav">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="">
-            <span class="fa fa-stack" style="color:#fff">
-                <i class="fa fa-square-o fa-stack-2x"></i>
-                <i class="fa fa-home fa-stack-1x"></i>
-            </span>
-        </a>
-    </div>
-<div class="collapse navbar-collapse" id="mynav">
-    <ul class="nav navbar-nav">
-      <!-- //////////////////////////////////////Menu 1 start//////////////////////////////////////////////////<-->
-        <li class="dropdown">
-            <a data-toggle="dropdown">
-                <span class="fa fa-user-secret"></span>
-                <sup><span class="fa fa-briefcase"></span></sup>
-                Manage Users <span class="badge bg-warning myOrderBadge">0</span> <i class="fa fa-caret-down"></i>
-            </a>
-            <ul class="dropdown-menu">
-                <li>
-                    <a onclick="menuClicked('Manage Staff')"><span class="fa fa-stack">
-                            <i class="fa fa-stack-1x fa-user-secret"></i>
-                            <i class="fa fa-stack-2x fa-square-o"></i>
-                        </span>
-                        Manage Staff
-                    </a>
-                </li>
-                <li>
-                    <a>
-                        <span class="fa fa-stack">
-                            <i class="fa fa-stack-1x fa-user-o"></i>
-                            <i class="fa fa-stack-2x fa-square-o"></i>
-                        </span>
-                        Manage Clients <span class="badge bg-warning myOrderBadge">0</span>
-                    </a>
-                </li>
-            </ul>
-        </li>
-         <li class="dropdown">
-            <a data-toggle="dropdown">
-                <span class="fa fa-user-secret"></span>
-                <sup><span class="fa fa-briefcase"></span></sup>
-                Products Manager <i class="fa fa-caret-down"></i>
-            </a>
-            <ul class="dropdown-menu">
-                <li>
-                    <a onclick="menuClicked('goodsManager')"><span class="fa fa-stack">
-                            <i class="fa fa-stack-1x fa-briefcase"></i>
-                            <i class="fa fa-stack-2x fa-square-o"></i>
-                        </span>
-                       Manage my Products
-                    </a>
-                </li>
-                <li>
-                    <a onclick="menuClicked('my Store')">
-                        <span class="fa fa-stack">
-                            <i class="fa fa-stack-1x fa-book"></i>
-                            <i class="fa fa-stack-2x fa-square-o"></i>
-                        </span>
-                        My Store
-                    </a>
-                </li>
-                <li>
-                    <a onclick="menuClicked('pickup manager')">
-                        <span class="fa fa-stack">
-                            <i class="fa fa-stack-1x fa-fort-awesome"></i>
-                            <i class="fa fa-stack-2x fa-square-o"></i>
-                        </span>
-                        Pick-up Points
-                    </a>
-                </li>
-            </ul>
-        </li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown">
+        <div class="container-fluid bg1">
+            <div class="nav navbar-header">
+                <button type="button" data-toggle="collapse" class="navbar-toggle" data-target="#mynav">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="">
+                    <span class="fa fa-stack" style="color:#fff">
+                        <i class="fa fa-square-o fa-stack-2x"></i>
+                        <i class="fa fa-home fa-stack-1x"></i>
+                    </span>
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="mynav">
+                <ul class="nav navbar-nav">
+                    <!-- //////////////////////////////////////Menu 1 start//////////////////////////////////////////////////<-->
+                    <li class="dropdown">
+                        <a data-toggle="dropdown">
+                            <span class="fa fa-user-secret"></span>
+                            <sup><span class="fa fa-briefcase"></span></sup>
+                            Manage Users <span class="badge bg-warning myOrderBadge">0</span> <i class="fa fa-caret-down"></i>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a onclick="menuClicked('Manage Staff')"><span class="fa fa-stack">
+                                        <i class="fa fa-stack-1x fa-user-secret"></i>
+                                        <i class="fa fa-stack-2x fa-square-o"></i>
+                                    </span>
+                                    Manage Staff
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    <span class="fa fa-stack">
+                                        <i class="fa fa-stack-1x fa-user-o"></i>
+                                        <i class="fa fa-stack-2x fa-square-o"></i>
+                                    </span>
+                                    Manage Clients <span class="badge bg-warning myOrderBadge">0</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a data-toggle="dropdown">
+                            <span class="fa fa-user-secret"></span>
+                            <sup><span class="fa fa-briefcase"></span></sup>
+                            Products Manager <i class="fa fa-caret-down"></i>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a onclick="menuClicked('goodsManager')"><span class="fa fa-stack">
+                                        <i class="fa fa-stack-1x fa-briefcase"></i>
+                                        <i class="fa fa-stack-2x fa-square-o"></i>
+                                    </span>
+                                    Manage my Products
+                                </a>
+                            </li>
+                            <li>
+                                <a onclick="menuClicked('my Store')">
+                                    <span class="fa fa-stack">
+                                        <i class="fa fa-stack-1x fa-book"></i>
+                                        <i class="fa fa-stack-2x fa-square-o"></i>
+                                    </span>
+                                    My Store
+                                </a>
+                            </li>
+                            <li>
+                                <a onclick="menuClicked('pickup manager')">
+                                    <span class="fa fa-stack">
+                                        <i class="fa fa-stack-1x fa-fort-awesome"></i>
+                                        <i class="fa fa-stack-2x fa-square-o"></i>
+                                    </span>
+                                    Pick-up Points
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown">
 
-                <i class="fa fa-user-secret"></i>
+                            <i class="fa fa-user-secret"></i>
 
-                User : <strong><?php echo $_SESSION['marvel']; ?></strong>
-                <span class="glyphicon glyphicon-chevron-down"></span>
-            </a>
-            <ul class="dropdown-menu">
-                <li>
-                    <a onclick="myAccount('<?php echo $_SESSION['marvel']?>')">
-                        <i class="fa fa-user-o"></i>
-                         My Account
-                    </a>
-                </li>
-                <li>
-                    <a href="@logout">
-                        <span class="glyphicon glyphicon-log-out"></span> 
-                        Logout
-                    </a>
-                </li>
-            </ul>
-        </li>
-    </ul>
-</div>
+                            User : <strong><?php echo $_SESSION['marvel']; ?></strong>
+                            <span class="glyphicon glyphicon-chevron-down"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a onclick="myAccount('<?php echo $_SESSION['marvel'] ?>')">
+                                    <i class="fa fa-user-o"></i>
+                                    My Account
+                                </a>
+                            </li>
+                            <li>
+                                <a href="@logout">
+                                    <span class="glyphicon glyphicon-log-out"></span> 
+                                    Logout
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 
-  
+
     <div class="container-fluid mydiv" id="mainDivDboard">
-        
+
     </div>
-    <?php require './includes/myAccount.php';?>
+    <?php require './includes/myAccount.php'; ?>
 </body>
 </html>
--- @garladoSys/JS/simpleQuery.js
+++ @garladoSys/JS/simpleQuery.js
@@ -1,16 +1,59 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // this function loads new orders and places them here
     checkOrder();
+    //  Option manage client
+    $('#mynav .dropdown:eq(0) ul li:eq(1) a').click(function () {
+        LoadClients();
+    });
 });
 
-function checkOrder(){
+function checkOrder() {
     var spin = '<i class="fa fa-pulse fa-refresh"></i>';
     $('#mynav .myOrderBadge').html(spin);
     $.post('AjaxPhp/simpleQuery.php',
-    {
-        cat:'checkOrder'
-    },
-    function(data,status){
-        $('#mynav .myOrderBadge').html(data);
+            {
+                cat: 'checkOrder'
+            },
+            function (data, status) {
+                $('#mynav .myOrderBadge').html(data);
+            });
+}
+
+function LoadClients() {
+    $.get('widgets/@manageOrders.html', function (data, status) {
+        $('#mainDivDboard').html(data);
+        //asign click listeners here also
+        /*
+         *1.  New Orders Click Registration 
+         */
+        $('#orderManagement li:eq(0) a').click(function () {
+            loadNewOrders();
+        });
+        $('#orderManagement li:eq(1),#orderManagement li:eq(1) a').click(function () {
+            loadPendingOrders();
+        });
+        $('#orderManagement li:eq(2),#orderManagement li:eq(2) a').click(function () {
+            loadClosedOrders();
+        });
+    });
+
+
+}
+
+function loadNewOrders() {
+    $.get('widgets/Orders/newOrders.html', function (data, status) {
+        $('#holder').html(data);
+    });
+}
+
+function loadPendingOrders() {
+    $.get('widgets/Orders/pendingOrders.html', function (data, status) {
+        $('#holder').html(data);
+    });
+}
+
+function loadClosedOrders() {
+    $.get('widgets/Orders/closedOrders.html', function (data, status) {
+        $('#holder').html(data);
     });
 }
