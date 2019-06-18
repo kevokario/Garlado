facebccef18b71e524df38714ef9c58091604e3a
Kevo Kario <kevokario@gmail.com>
2019-06-18 19:54:49 +0300

    Work in progress

--- @garladoSys/JS/simpleQuery.js
+++ @garladoSys/JS/simpleQuery.js
@@ -1,98 +1,96 @@
 $(document).ready(function () {
-    // this function loads new orders and places them here
-    checkOrder();
-    //  Option manage client
-    $('#mynav .dropdown:eq(0) ul li:eq(1) a').click(function () {
-        LoadClients();
-    });
+// this function loads new orders and places them here
+checkOrder();
+        //  Option manage client
+        $('#mynav .dropdown:eq(0) ul li:eq(1) a').click(function () {
+LoadClients();
 });
-
-function checkOrder() {
-    var spin = '<i class="fa fa-pulse fa-refresh"></i>';
-    $('#mynav .myOrderBadge').html(spin);
-    $.post('AjaxPhp/simpleQuery.php',
-            {
+        });
+        function checkOrder() {
+        var spin = '<i class="fa fa-pulse fa-refresh"></i>';
+                $('#mynav .myOrderBadge').html(spin);
+                $.post('AjaxPhp/simpleQuery.php',
+                {
                 cat: 'checkOrder'
-            },
-            function (data, status) {
-                $('#mynav .myOrderBadge').html(data);
-            });
-}
+                },
+                        function (data, status) {
+                        $('#mynav .myOrderBadge').html(data);
+                        });
+                }
 
 function LoadClients() {
-    loader($('#mainDivDboard'));
-    $.get('widgets/@manageOrders.html', function (data, status) {
+loader($('#mainDivDboard'));
+        $.get('widgets/@manageOrders.html', function (data, status) {
         $('#mainDivDboard').html(data);
-        //asign click listeners here also
-        /*
-         *1.  New Orders Click Registration 
-         */
-        $('#orderManagement li:eq(0) a').click(function () {
-            loadNewOrders();
+                //asign click listeners here also
+                /*
+                 *1.  New Orders Click Registration 
+                 */
+                $('#orderManagement li:eq(0) a').click(function () {
+        loadNewOrders();
         });
-        $('#orderManagement li:eq(1),#orderManagement li:eq(1) a').click(function () {
-            loadPendingOrders();
+                $('#orderManagement li:eq(1),#orderManagement li:eq(1) a').click(function () {
+        loadPendingOrders();
         });
-        $('#orderManagement li:eq(2),#orderManagement li:eq(2) a').click(function () {
-            loadClosedOrders();
+                $('#orderManagement li:eq(2),#orderManagement li:eq(2) a').click(function () {
+        loadClosedOrders();
         });
-    });
-
-
-}
+        });
+        }
 
 function loadNewOrders() {
-    loader($('#holder'));
-    $.get('widgets/Orders/newOrders.html', function (data, status) {
+loader($('#holder'));
+        $.get('widgets/Orders/newOrders.html', function (data, status) {
         $('#holder').html(data);
-        loadOrders();
-    });
-}
+                loadOrders();
+        });
+        }
 
 function loadPendingOrders() {
-    loader($('#holder'));
-    $.get('widgets/Orders/pendingOrders.html', function (data, status) {
+loader($('#holder'));
+        $.get('widgets/Orders/pendingOrders.html', function (data, status) {
         $('#holder').html(data);
-    });
-}
+        });
+        }
 
 function loadClosedOrders() {
-    loader($('#holder'));
-    $.get('widgets/Orders/closedOrders.html', function (data, status) {
+loader($('#holder'));
+        $.get('widgets/Orders/closedOrders.html', function (data, status) {
         $('#holder').html(data);
-    });
-}
+        });
+        }
 
 function loader(div) {
-    $(div).html('<p class="text-center" style="margin-bottom:0; margin: 10px"><i class="fa fa-pulse fa-refresh"></i></p>');
-}
+$(div).html('<p class="text-center" style="margin-bottom:0; margin: 10px"><i class="fa fa-pulse fa-refresh"></i></p>');
+        }
 ;
-
-
-function loadOrders() {
-    loader($('#newOrderList'));
-    $.post('AjaxPhp/simpleQuery.php',
-            {
+        function loadOrders() {
+        loader($('#newOrderList'));
+                $.post('AjaxPhp/simpleQuery.php',
+                {
                 cat: 'loadNewOrders'
-            },
-            function (data,status) {
-                $('#newOrderList').html(data);
-                $('#newOrderList tr button').click(function(){
-                    var orderNumber = $(this).parent().parent().children('td:eq(1)').text();
-                    openOrder(orderNumber);
-                });
-            }
-    );
-}
+                },
+                        function (data, status) {
+                        $('#newOrderList').html(data);
+                                $('#newOrderList tr button').click(function(){
+                        var orderNumber = $(this).parent().parent().children('td:eq(1)').text();
+                                openOrder(orderNumber);
+                        });
+                        }
+                );
+                }
 
 //this function registers click function to button on new orders table!
 
 function openOrder(str){
-    $.post('AjaxPhp/simpleQuery.php',{
-        cat:'getOrderItems',
+$.post('AjaxPhp/simpleQuery.php', {
+cat:'getOrderItems',
         orderNumber:str.trim()
-    },function(data,status){
+}, function(data, status){
 //        test(data);
-       $('#openOrdersModal').modal('show'); 
-    });
-}
+var url = "https://maps.google.com/maps?q=quest%20website%20developers&t=&z=13&ie=UTF8&iwloc=&output=embed";
+        var urldest = "https://www.google.com/maps/embed/v1/directions?origin=40.7127837,-74.0059413&destination=42.3600825,-71.05888&key=AIzaSyDzlJ68ZHuGRQ6eFfFtGFwNchJG98CRE-g";
+        $('#openOrdersModal #iframe').attr('src', urldest);
+        $('#openOrdersModal').modal('show');
+});
+        }
--- @garladoSys/includes/header.php
+++ @garladoSys/includes/header.php
@@ -7,13 +7,17 @@
         <link rel="stylesheet" href="../Res/bs/css/bootstrap.min.css"/>
         <link rel="stylesheet" href="../Res/bs/css/bootstrap-theme.min.css"/>
         <link rel="stylesheet" href="../Res/fa/css/font-awesome.min.css"/>
-    
+
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+        <meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Expires" content="0">
+
         <script src="../Res/bs/js/jquery-3.3.1.js"></script>
         <script src="../Res/bs/js/bootstrap.min.js"></script>
         <script src="../Res/Chartjs/Chart.min.js" type="text/javascript"></script>
         <script src="../Res/bs/js/bootstrapValidator.min.js"></script>
         <link rel="icon" href="img/logo.png"/>
-    
+
         <script src="Ajax/loaderAjax.js" type="text/javascript"></script>
         <script src="Ajax/authAjax.js" type="text/javascript"></script>
         <script src="Ajax/authPhp.js" type="text/javascript"></script>
